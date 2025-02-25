4e10690446e00b24eb8c778e49a2f4640c5335c6
MahdiKooshafar <mkooshafar2@gmail.com>
2025-02-25 01:09:42 -0800

    not correct

--- Persistence/User/UserRepository.cs
+++ Persistence/User/UserRepository.cs
@@ -1,42 +1,42 @@
-﻿//using CBG;
-//using Domain.Aggregates.User;
-//using Domain.Aggregates.User.ValueObjects;
-//using DTOs.User;
-//using Microsoft.EntityFrameworkCore;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using CBG;
+using Domain.Aggregates.User;
+using Domain.Aggregates.User.ValueObjects;
+using DTOs.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Persistence.User
-//{
-//    public class UserRepository : Repository<Domain.Aggregates.User.User>, IUserRepository
-//    {
-//        public UserRepository(DataBaseContext databaseContext) : base(databaseContext: databaseContext)
-//        {
-            
-//        }
-//        public async Task<UserVeiwModel> GetByUserNameAsync(UserName username, CancellationToken cancellationToken)
-//        {
+namespace Persistence.User
+{
+    public class UserRepository : Repository<Domain.Aggregates.User.User>, IUserRepository
+    {
+        public UserRepository(DataBaseContext databaseContext) : base(databaseContext: databaseContext)
+        {
 
-//            var result =
-//                await
-//                DbSet
-//                .Where(current => current.UserName == username)
-//                .Select(current => new UserVeiwModel
-//                {
-//                    Id = current.Id,
-//                    FirstName = current.FirstName,
-//                    LastName = current.LastName,
-//                    UserName = current.UserName,
-//                    Password = current.Password
-//                })
-//                .FirstOrDefaultAsync(cancellationToken: cancellationToken);
+        }
+        public async Task<UserVeiwModel> GetByUserNameAsync(UserName username, CancellationToken cancellationToken)
+        {
 
-//            return result;
-//        }
-//    }
+            var result =
+                await
+                DbSet
+                .Where(current => current.UserName == username)
+                .Select(current => new UserVeiwModel
+                {
+                    Id = current.Id,
+                    FirstName = current.FirstName,
+                    LastName = current.LastName,
+                    UserName = current.UserName,
+                    Password = current.Password
+                })
+                .FirstOrDefaultAsync(cancellationToken: cancellationToken);
+
+            return result;
+        }
+    }
 
 
-//}
+}
